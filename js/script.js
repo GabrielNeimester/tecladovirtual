1ba206adedff918a1fa463df2374d33ec70728f3
Gabriel Neimester <gneimester@hotmail.com>
2022-11-02 14:57:19 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const botoes = document.getElementById('teclado')
+let senhatv =''
+
+document.getElementById('senha').addEventListener('keypress', function(e) {
+    if (e.key != '') {
+        e.preventDefault()
+        alert('Por favor utilize o teclado virtual!');
+    }
+});
+
+function inserirsenha(valor){
+    if (valor == 10){
+        senhatv =""
+        valor = ""
+    }
+    else if(valor == 11 ){
+        valor=""
+        if(senhatv.length >= 4 && senhatv.length <= 6){
+        alert("A senha é válida!")}
+        else{
+            alert("A senha não é válida")
+        }
+    } 
+    else if(senhatv.length > 5){
+        alert('A senha deve ter no máximo 6 números!')
+        valor=""
+    }
+    senhatv = senhatv + valor
+    document.getElementById('senha').value = senhatv 
+  }
